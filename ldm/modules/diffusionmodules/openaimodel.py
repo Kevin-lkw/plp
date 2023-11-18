5c93b2995c7a9513a8c7081919f42020c5423542
En-2863 <ewea20221310@gmail.com>
2023-11-18 23:27:12 +0800

    Add outer time embedding

--- ldm/modules/diffusionmodules/openaimodel.py
+++ ldm/modules/diffusionmodules/openaimodel.py
@@ -269,7 +269,12 @@ class ResBlock(TimestepBlock):
             h = out_norm(h) * (1 + scale) + shift
             h = out_rest(h)
         else:
-            h = h + emb_out
+            if emb.size(0) == h.size(0): # For fixed stable diffusion
+                h = h + emb
+            else:                        # For control net
+                emb_out_T, emb_in_T = th.chunk(emb_out, 2, dim=0)
+                h = h + emb_out_T + emb_in_T
+            
             h = self.out_layers(h)
         return self.skip_connection(x) + h
 
